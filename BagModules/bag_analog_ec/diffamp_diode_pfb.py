6ea6504987754ae1ff2276b56c5d26d2c11982f8
Eric Chang <pkerichang@gmail.com>
2018-02-08 09:52:18 -0800

    1. update diffamp_diode_pfb schematic generator to use latest API. 2. added diffamp_self_biased schematic generator and templates.

--- BagModules/bag_analog_ec/diffamp_diode_pfb.py
+++ BagModules/bag_analog_ec/diffamp_diode_pfb.py
@@ -7,7 +7,6 @@ import pkg_resources
 
 from bag.design import Module
 
-
 yaml_file = pkg_resources.resource_filename(__name__, os.path.join('netlist_info', 'diffamp_diode_pfb.yaml'))
 
 
@@ -37,58 +36,26 @@ class bag_analog_ec__diffamp_diode_pfb(Module):
             th_dict='Dictionary of transistor threshold.',
             seg_dict='Dictionary of transistor number of segments.',
             stack_dict='Dictionary of transistor stack count.',
-            dum_dict='Dictionary of dummies.',
+            dum_info='Dummy information data structure.',
         )
 
-    def design(self, lch, w_dict, th_dict, seg_dict, stack_dict, dum_dict):
+    def design(self, lch, w_dict, th_dict, seg_dict, stack_dict, dum_info):
         """Design the differential amplifier with diode/positive-feedback load.
         """
+        # design transistors
+        tran_info_list = [('XTAIL', 'tail', 'tail'), ('XREF', 'ref', 'tail'),
+                          ('XINL', 'in', 'in'), ('XINR', 'in', 'in'), ('XRES', 'ref', 'in'),
+                          ('XDIOL', 'diode', 'load'), ('XDIOR', 'diode', 'load'),
+                          ('XNGML', 'ngm', 'load'), ('XNGMR', 'ngm', 'load'),
+                          ]
+        for inst_name, inst_type, row_type in tran_info_list:
+            w = w_dict[row_type]
+            th = th_dict[row_type]
+            seg = seg_dict[inst_type]
+            stack = stack_dict.get(inst_type, stack_dict[row_type])
+            if inst_name == 'XTAIL':
+                seg *= 2
+            self.instances[inst_name].design(w=w, l=lch, seg=seg, intent=th, stack=stack)
+
         # design dummies
-        w_tail = w_dict['tail']
-        w_in = w_dict['in']
-        w_load = w_dict['load']
-        th_tail = th_dict['tail']
-        th_in = th_dict['in']
-        th_load = th_dict['load']
-        ndum_tail = dum_dict['tail']
-        ndum_in = dum_dict['in']
-        ndum_load = dum_dict['load']
-        pdum_list = []
-        if ndum_tail > 0:
-            pdum_list.append((w_tail, th_tail, ndum_tail))
-        if ndum_in > 0:
-            pdum_list.append((w_in, th_in, ndum_in))
-
-        # array pmos dummies and design
-        self.array_instance('XPD', ['XPDD%s' % idx for idx in range(len(pdum_list))])
-        for inst, (w_cur, th_cur, ndum_cur) in zip(self.instances['XPD'], pdum_list):
-            inst.design(w=w_cur, l=lch, intent=th_cur, nf=ndum_cur)
-
-        # design rest of the dummies
-        for name in ('XPD1', 'XPD2', 'XPD3'):
-            self.instances[name].design(w=w_in, l=lch, intent=th_in, nf=2)
-        for name in ('XND1', 'XND2'):
-            self.instances[name].design(w=w_load, l=lch, intent=th_load, nf=2)
-        self.instances['XND'].design(w=w_load, l=lch, intent=th_load, nf=ndum_load)
-
-        # design main transistors
-        seg_tail = seg_dict['tail']
-        seg_in = seg_dict['in']
-        seg_ref = seg_dict['ref']
-        seg_diode = seg_dict['diode']
-        seg_ngm = seg_dict['ngm']
-
-        stack_tail = stack_dict['tail']
-        stack_in = stack_dict['in']
-        stack_diode = stack_dict['diode']
-        stack_ngm = stack_dict['ngm']
-
-        self.instances['XTAIL'].design(w=w_tail, l=lch, seg=seg_tail * 2, intent=th_tail, stack=stack_tail)
-        self.instances['XREF'].design(w=w_tail, l=lch, seg=seg_ref, intent=th_tail, stack=stack_tail)
-        self.instances['XINL'].design(w=w_in, l=lch, seg=seg_in, intent=th_in, stack=stack_in)
-        self.instances['XINR'].design(w=w_in, l=lch, seg=seg_in, intent=th_in, stack=stack_in)
-        self.instances['XRES'].design(w=w_in, l=lch, seg=seg_ref, intent=th_in, stack=stack_in)
-        self.instances['XDIOL'].design(w=w_load, l=lch, seg=seg_diode, intent=th_load, stack=stack_diode)
-        self.instances['XDIOR'].design(w=w_load, l=lch, seg=seg_diode, intent=th_load, stack=stack_diode)
-        self.instances['XNGML'].design(w=w_load, l=lch, seg=seg_ngm, intent=th_load, stack=stack_ngm)
-        self.instances['XNGMR'].design(w=w_load, l=lch, seg=seg_ngm, intent=th_load, stack=stack_ngm)
+        self.design_dummy_transistors(dum_info, 'XDUM', 'VDD', 'VSS')
---
+++ BagModules/bag_analog_ec/diffamp_self_biased.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+from typing import Dict
+
+import os
+import pkg_resources
+
+from bag.design import Module
+
+yaml_file = pkg_resources.resource_filename(__name__, os.path.join('netlist_info', 'diffamp_self_biased.yaml'))
+
+
+# noinspection PyPep8Naming
+class bag_analog_ec__diffamp_self_biased(Module):
+    """Module for library bag_analog_ec cell diffamp_self_biased.
+
+    This is a self-biased differential amplifier with single-ended output.
+    """
+
+    def __init__(self, bag_config, parent=None, prj=None, **kwargs):
+        Module.__init__(self, bag_config, yaml_file, parent=parent, prj=prj, **kwargs)
+
+    @classmethod
+    def get_params_info(cls):
+        # type: () -> Dict[str, str]
+        """Returns a dictionary from parameter names to descriptions.
+
+        Returns
+        -------
+        param_info : Optional[Dict[str, str]]
+            dictionary from parameter names to descriptions.
+        """
+        return dict(
+            lch='Channel length, in meters.',
+            w_dict='Transistor width dictionary.',
+            th_dict='Transistor threshold dictionary.',
+            seg_dict='Transistor number of segments dictionary.',
+            dum_info='Dummy information data structure.',
+        )
+
+    def design(self, lch, w_dict, th_dict, seg_dict, dum_info):
+        # design main transistors
+        tran_info_list = [('XNTAIL1', 'ntail'), ('XNTAIL2', 'ntail'),
+                          ('XNINP', 'nin'), ('XNINN', 'nin'),
+                          ('XPINP', 'pin'), ('XPINN', 'nin'),
+                          ('XPTAIL1', 'ptail'), ('XPTAIL2', 'ptail'),
+                          ]
+
+        for inst_name, inst_type in tran_info_list:
+            w = w_dict[inst_type]
+            th = th_dict[inst_type]
+            seg = seg_dict[inst_type]
+            stack = 1
+            self.instances[inst_name].design(w=w, l=lch, seg=seg, intent=th, stack=stack)
+
+        # design dummies
+        self.design_dummy_transistors(dum_info, 'XDUM', 'VDD', 'VSS')
