1fb5f6555d7438c52341c0de52b968972ce9dce7
Eric Chang <pkerichang@gmail.com>
2018-02-12 18:04:10 -0800

    1. fix error in res_termination_cm script.

--- scripts_test/res_termination_cm.py
+++ scripts_test/res_termination_cm.py
@@ -5,7 +5,7 @@ import yaml
 from bag.core import BagProject
 from bag.layout import RoutingGrid, TemplateDB
 
-from analog_ec.layout.passives.resistor import TerminationCMCore
+from analog_ec.layout.passives.resistor.termination import TerminationCMCore
 
 
 def make_tdb(prj, target_lib, specs):
