b52eb85085070d991af29fa9c2364c9e8bd52ecc
Eric Chang <pkerichang@gmail.com>
2018-04-11 15:41:34 -0700

    update clk_amp_reset script to conform to latest standard.

--- scripts_test/clk_amp_reset.py
+++ scripts_test/clk_amp_reset.py
@@ -2,54 +2,9 @@
 
 import yaml
 
-import bag
-from bag.layout import RoutingGrid, TemplateDB
+from bag.core import BagProject
+
 from analog_ec.layout.clk.driver import ClkAmpReset
-
-
-def make_tdb(prj, target_lib, specs):
-    grid_specs = specs['routing_grid']
-    layers = grid_specs['layers']
-    spaces = grid_specs['spaces']
-    widths = grid_specs['widths']
-    bot_dir = grid_specs['bot_dir']
-
-    routing_grid = RoutingGrid(prj.tech_info, layers, spaces, widths, bot_dir)
-    tdb = TemplateDB('template_libs.def', routing_grid, target_lib, use_cybagoa=True)
-    return tdb
-
-
-def generate(prj, specs, gen_sch=False, run_lvs=False):
-    impl_lib = specs['impl_lib']
-    impl_cell = specs['impl_cell']
-    sch_lib = specs['sch_lib']
-    sch_cell = specs['sch_cell']
-
-    params = specs['params']
-
-    temp_db = make_tdb(prj, impl_lib, specs)
-    template = temp_db.new_template(params=params, temp_cls=ClkAmpReset)
-
-    print('creating layouts')
-    temp_db.batch_layout(prj, [template], [impl_cell])
-    print('layout done.')
-
-    if gen_sch:
-        print('creating schematics')
-
-        dsn = prj.create_design_module(sch_lib, sch_cell)
-        dsn.design(**template.sch_params)
-        dsn.implement_design(impl_lib, top_cell_name=impl_cell)
-        print('schematic done.')
-
-        if run_lvs:
-            print('running lvs')
-            lvs_passed, lvs_log = prj.run_lvs(impl_lib, impl_cell)
-            print('LVS log: %s' % lvs_log)
-            if lvs_passed:
-                print('LVS passed!')
-            else:
-                print('LVS failed...')
 
 
 if __name__ == '__main__':
@@ -60,11 +15,11 @@ if __name__ == '__main__':
     local_dict = locals()
     if 'bprj' not in local_dict:
         print('creating BAG project')
-        bprj = bag.BagProject()
+        bprj = BagProject()
 
     else:
         print('loading BAG project')
         bprj = local_dict['bprj']
 
-    # generate(bprj, block_specs, gen_sch=False, run_lvs=False)
-    generate(bprj, block_specs, gen_sch=True, run_lvs=False)
+    # bprj.generate_cell(block_specs, ClkAmpReset, gen_sch=False, run_lvs=False, use_cybagoa=True)
+    bprj.generate_cell(block_specs, ClkAmpReset, gen_sch=True, run_lvs=True, use_cybagoa=True)
