e59b36d009585695685201112c6220e89e5c45de
Eric Chang <pkerichang@gmail.com>
2018-04-11 16:47:48 -0700

    ResSubstrateWrapper: 1. fix how substrate contacts are instantiated next to resistor array    to avoid bugs with half-integer block sizes.

--- analog_ec/layout/passives/resistor/base.py
+++ analog_ec/layout/passives/resistor/base.py
@@ -4,6 +4,7 @@ from typing import TYPE_CHECKING, Dict, Any, Set
 
 import importlib
 
+from bag.layout.util import BBox
 from bag.layout.template import TemplateBase
 
 from abs_templates_ec.analog_core.substrate import SubstrateContact
@@ -82,6 +83,8 @@ class ResSubstrateWrapper(TemplateBase):
         params['show_pins'] = False
         res_master = self.new_template(params=params, temp_cls=temp_cls)
         self._sch_params = res_master.sch_params.copy()
+        top_layer = res_master.top_layer
+        res_box = res_master.bound_box
 
         if sub_w == 0:
             # do not draw substrate contact.
@@ -89,11 +92,12 @@ class ResSubstrateWrapper(TemplateBase):
             for port_name in inst.port_names_iter():
                 self.reexport(inst.get_port(port_name), show=show_pins)
             self.array_box = inst.array_box
-            self.set_size_from_bound_box(res_master.top_layer, res_master.bound_box)
+            self.set_size_from_bound_box(top_layer, res_box)
         else:
+            res = self.grid.resolution
+            blkw, blkh = self.grid.get_block_size(top_layer, unit_mode=True)
+
             # draw contact and move array up
-            top_layer, nx_arr, ny_arr = res_master.size
-            w_pitch, h_pitch = self.grid.get_size_pitch(top_layer, unit_mode=True)
             sub_params = dict(
                 top_layer=top_layer,
                 lch=sub_lch,
@@ -102,23 +106,20 @@ class ResSubstrateWrapper(TemplateBase):
                 threshold=params['threshold'],
                 port_width=sub_tr_w,
                 well_width=res_master.get_well_width(),
+                max_nxblk=res_box.width_unit // blkw,
                 show_pins=False,
                 is_passive=True,
-                tot_width_parity=nx_arr % 2,
             )
             sub_master = self.new_template(params=sub_params, temp_cls=SubstrateContact)
             sub_box = sub_master.bound_box
-            ny_shift = -(-sub_box.height_unit // h_pitch)
+            sub_x = (res_box.width_unit - sub_box.width_unit) // 2
 
             # compute substrate X coordinate so substrate is on its own private horizontal pitch
-            sub_x_pitch, _ = sub_master.grid.get_size_pitch(sub_master.size[0], unit_mode=True)
-            sub_x = ((w_pitch * nx_arr - sub_box.width_unit) // 2 // sub_x_pitch) * sub_x_pitch
-
             bot_inst = self.add_instance(sub_master, inst_name='XBSUB', loc=(sub_x, 0),
                                          unit_mode=True)
             res_inst = self.add_instance(res_master, inst_name='XRES',
-                                         loc=(0, ny_shift * h_pitch), unit_mode=True)
-            top_yo = (ny_arr + 2 * ny_shift) * h_pitch
+                                         loc=(0, sub_box.height_unit), unit_mode=True)
+            top_yo = sub_box.height_unit * 2 + res_box.height_unit
             top_inst = self.add_instance(sub_master, inst_name='XTSUB', loc=(sub_x, top_yo),
                                          orient='MX', unit_mode=True)
 
@@ -130,9 +131,12 @@ class ResSubstrateWrapper(TemplateBase):
             sub_port_name = 'VDD' if sub_type == 'ntap' else 'VSS'
             self.reexport(bot_inst.get_port(sub_port_name), show=show_pins)
             self.reexport(top_inst.get_port(sub_port_name), show=show_pins)
-            self.size = top_layer, nx_arr, ny_arr + 2 * ny_shift
-            self.array_box = bot_inst.array_box.merge(top_inst.array_box)
-            self.add_cell_boundary(self.bound_box)
+            arr_box = BBox(0, bot_inst.array_box.bottom_unit, res_inst.bound_box.right_unit,
+                           top_inst.array_box.top_unit, res, unit_mode=True)
+            bnd_box = arr_box.extend(y=0, unit_mode=True).extend(y=top_yo, unit_mode=True)
+            self.array_box = arr_box
+            self.set_size_from_bound_box(top_layer, bnd_box)
+            self.add_cell_boundary(bnd_box)
 
             for port_name in res_inst.port_names_iter():
                 self.reexport(res_inst.get_port(port_name), show=show_pins)
