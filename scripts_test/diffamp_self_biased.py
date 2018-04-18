9ba29d485ed5c0df8f44fe5dba77026aead27bd9
Eric Chang <pkerichang@gmail.com>
2018-04-18 03:53:41 -0700

    update self-biased diffamp test script.

--- scripts_test/diffamp_self_biased.py
+++ scripts_test/diffamp_self_biased.py
@@ -3,53 +3,7 @@
 import yaml
 
 import bag
-from bag.layout import RoutingGrid, TemplateDB
 from analog_ec.layout.amplifiers.diffamp import DiffAmpSelfBiased
-
-
-def make_tdb(prj, target_lib, specs):
-    grid_specs = specs['routing_grid']
-    layers = grid_specs['layers']
-    widths = grid_specs['widths']
-    spaces = grid_specs['spaces']
-    bot_dir = grid_specs['bot_dir']
-    width_override = grid_specs.get('width_override', None)
-
-    routing_grid = RoutingGrid(prj.tech_info, layers, spaces, widths, bot_dir,
-                               width_override=width_override)
-    tdb = TemplateDB('template_libs.def', routing_grid, target_lib, use_cybagoa=True)
-    return tdb
-
-
-def generate(prj, specs, gen_sch=False, run_lvs=False):
-    impl_lib = specs['impl_lib']
-    impl_cell = specs['impl_cell']
-    sch_lib = specs['sch_lib']
-    sch_cell = specs['sch_cell']
-    params = specs['params']
-
-    temp_db = make_tdb(prj, impl_lib, specs)
-    template = temp_db.new_template(params=params, temp_cls=DiffAmpSelfBiased)
-
-    print('creating layouts')
-    temp_db.batch_layout(prj, [template], [impl_cell])
-    print('layout done.')
-
-    if gen_sch:
-        dsn = prj.create_design_module(sch_lib, sch_cell)
-        dsn.design(**template.sch_params)
-        print('creating schematics')
-        dsn.implement_design(impl_lib, top_cell_name=impl_cell)
-        print('schematic done.')
-
-    if run_lvs:
-        print('running lvs')
-        lvs_passed, lvs_log = prj.run_lvs(impl_lib, impl_cell)
-        print('LVS log: %s' % lvs_log)
-        if lvs_passed:
-            print('LVS passed!')
-        else:
-            print('LVS failed...')
 
 
 if __name__ == '__main__':
@@ -65,5 +19,6 @@ if __name__ == '__main__':
         print('loading BAG project')
         bprj = local_dict['bprj']
 
-    # generate(bprj, block_specs, gen_sch=False, run_lvs=False)
-    generate(bprj, block_specs, gen_sch=True, run_lvs=True)
+    gds_lay_file = 'GF45RFSOI_gds_info.yaml'
+    bprj.generate_cell(block_specs, DiffAmpSelfBiased, gds_lay_file=gds_lay_file, debug=True)
+    # bprj.generate_cell(block_specs, DiffAmpSelfBiased, use_cybagoa=True, debug=True)
