c80f97e1109c9bf7de302aae86168ef13e92f0cb
Eric Chang <pkerichang@gmail.com>
2018-05-07 05:42:48 -0700

    1. added high-pass filter array.

--- analog_ec/layout/passives/filter/highpass.py
+++ analog_ec/layout/passives/filter/highpass.py
@@ -416,3 +416,117 @@ class HighPassDiff(SubstrateWrapper):
         self.draw_layout_helper(HighPassDiffCore, params, sub_lch, sub_w, sub_tr_w, sub_type,
                                 threshold, show_pins, end_mode=end_mode, is_passive=True,
                                 sub_tids=sub_tids, )
+
+
+class HighPassArray(ResArrayBase):
+    """An array of RC high-pass filter.
+
+    Parameters
+    ----------
+    temp_db : :class:`bag.layout.template.TemplateDB`
+            the template database.
+    lib_name : str
+        the layout library name.
+    params : Dict[str, Any]
+        the parameter values.
+    used_names : Set[str]
+        a set of already used cell names.
+    **kwargs :
+        dictionary of optional parameters.  See documentation of
+        :class:`bag.layout.template.TemplateBase` for details.
+    """
+
+    def __init__(self, temp_db, lib_name, params, used_names, **kwargs):
+        # type: (TemplateDB, str, Dict[str, Any], Set[str], **kwargs) -> None
+        ResArrayBase.__init__(self, temp_db, lib_name, params, used_names, **kwargs)
+        self._sch_params = None
+
+    @property
+    def sch_params(self):
+        # type: () -> Dict[str, Any]
+        return self._sch_params
+
+    @classmethod
+    def get_params_info(cls):
+        # type: () -> Dict[str, str]
+        return dict(
+            w='unit resistor width, in meters.',
+            h_unit='total height, in resolution units.',
+            narr='Number of high-pass filters.',
+            sub_type='the substrate type.',
+            threshold='the substrate threshold flavor.',
+            top_layer='The top layer ID',
+            nser='number of resistors in series in a branch.',
+            ndum='number of dummy resistors.',
+            res_type='Resistor intent',
+            res_options='Configuration dictionary for ResArrayBase.',
+            cap_spx='Capacitor horizontal separation, in resolution units.',
+            cap_spy='Capacitor vertical space from resistor ports, in resolution units.',
+            cap_margin='Capacitor space from edge, in resolution units.',
+            half_blk_x='True to allow for half horizontal blocks.',
+            show_pins='True to show pins.',
+        )
+
+    @classmethod
+    def get_default_param_values(cls):
+        # type: () -> Dict[str, Any]
+        return dict(
+            res_type='standard',
+            res_options=None,
+            cap_spx=0,
+            cap_spy=0,
+            cap_margin=0,
+            half_blk_x=True,
+            show_pins=True,
+        )
+
+    def draw_layout(self):
+        # type: () -> None
+        w = self.params['w']
+        h_unit = self.params['h_unit']
+        narr = self.params['narr']
+        sub_type = self.params['sub_type']
+        threshold = self.params['threshold']
+        top_layer = self.params['top_layer']
+        nser = self.params['nser']
+        ndum = self.params['ndum']
+        res_type = self.params['res_type']
+        res_options = self.params['res_options']
+        cap_spx = self.params['cap_spx']
+        cap_spy = self.params['cap_spy']
+        cap_margin = self.params['cap_margin']
+        half_blk_x = self.params['half_blk_x']
+        show_pins = self.params['show_pins']
+
+        res = self.grid.resolution
+        lay_unit = self.grid.layout_unit
+        w_unit = int(round(w / lay_unit / res))
+
+        if res_options is None:
+            my_options = dict(well_end_mode=2)
+        else:
+            my_options = res_options.copy()
+            my_options['well_end_mode'] = 2
+        # find resistor length
+        info = ResArrayBaseInfo(self.grid, sub_type, threshold, top_layer=top_layer,
+                                res_type=res_type, ext_dir='y', options=my_options,
+                                connect_up=True, half_blk_x=half_blk_x, half_blk_y=True)
+
+        lmin, lmax = info.get_res_length_bounds()
+        bin_iter = BinaryIterator(lmin, lmax, step=2)
+        while bin_iter.has_next():
+            lcur = bin_iter.get_next()
+            htot = info.get_place_info(lcur, w_unit, 1, 1)[3]
+            if htot < h_unit:
+                bin_iter.save()
+                bin_iter.up()
+            else:
+                bin_iter.down()
+
+        # draw resistor
+        l_unit = bin_iter.get_last_save()
+        nx = 2 * ndum + narr * nser
+        self.draw_array(l_unit * lay_unit * res, w, sub_type, threshold, nx=nx, ny=1,
+                        top_layer=top_layer, res_type=res_type, grid_type=None, ext_dir='y',
+                        options=my_options, connect_up=True, half_blk_x=half_blk_x,
+                        half_blk_y=True, min_height=h_unit)
--- scripts_test/filter/hp_diff.py
+++ scripts_test/filter/hp_diff.py
@@ -8,7 +8,7 @@ from analog_ec.layout.passives.filter.highpass import HighPassDiff
 
 
 if __name__ == '__main__':
-    with open('specs_test/filter/highpass_diff.yaml', 'r') as f:
+    with open('specs_test/analog_ec/filter/hp_diff.yaml', 'r') as f:
         block_specs = yaml.load(f)
 
     local_dict = locals()
@@ -20,5 +20,5 @@ if __name__ == '__main__':
         print('loading BAG project')
         bprj = local_dict['bprj']
 
-    bprj.generate_cell(block_specs, HighPassDiff)
-    # bprj.generate_cell(block_specs, HighPassDiff, gen_sch=True, run_lvs=True)
+    bprj.generate_cell(block_specs, HighPassDiff, debug=True)
+    # bprj.generate_cell(block_specs, HighPassDiff, gen_sch=True, run_lvs=True, debug=True)
